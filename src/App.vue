90e5238550e1ec672db47bd5a3540c2b19b7849e
DanilapRock <b.danila21@yandex.ru>
2024-10-14 12:49:10 +0300

    2

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,7 @@
 import Header from "./components/Header.vue";
 </script>
 <template>
-<<<<<<< HEAD
-  <h1>Hi</h1>
-  <h2>Bye</h2>
-  <h3>minecraft</h3>
-  <p>i am tired</p>
-=======
   <body class="container-xl">
     <Header />
   </body>
->>>>>>> feature
 </template>
